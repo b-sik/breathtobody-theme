43992e416fa208a1c334de3d663d06c2bcc733a2
Brian Siklinski <bszyk.codes@gmail.com>
2022-03-12 21:31:28 -0500

    blog layout

--- resources/views/partials/content-single.blade.php
+++ resources/views/partials/content-single.blade.php
@@ -1,13 +1,8 @@
-<article @php post_class() @endphp>
-  <header>
+<article @php post_class('container py-5') @endphp>
+  <header class="pb-5">
     <h1 class="entry-title">{!! get_the_title() !!}</h1>
     @include('partials/entry-meta')
   </header>
   <div class="entry-content">
-    @php the_content() @endphp
-  </div>
-  <footer>
-    {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'breathtobody'), 'after' => '</p></nav>']) !!}
-  </footer>
-  @php comments_template('/partials/comments.blade.php') @endphp
+      @php the_content() @endphp
 </article>
--- resources/views/partials/content.blade.php
+++ resources/views/partials/content.blade.php
@@ -1,9 +1,11 @@
-<article @php post_class() @endphp>
-  <header>
-    <h2 class="entry-title"><a href="{{ get_permalink() }}">{!! get_the_title() !!}</a></h2>
-    @include('partials/entry-meta')
-  </header>
-  <div class="entry-summary">
-    @php the_excerpt() @endphp
-  </div>
+<article @php post_class('container pb-3 d-flex justify-content-center') @endphp>
+    <div class="col-8">
+        <header>
+            <h2 class="entry-title"><a href="{{ get_permalink() }}">{!! get_the_title() !!}</a></h2>
+            @include('partials/entry-meta')
+        </header>
+        <div class="entry-summary">
+            @php the_excerpt() @endphp
+        </div>
+    </div>
 </article>
